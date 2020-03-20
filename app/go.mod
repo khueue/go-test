08fa019fbf6f44205329cf4cdc305f376de09a06
Sebastian Lundström <khueue@gmail.com>
2020-03-20 16:44:32 +0100

    Try some imports

--- app/go.mod
+++ app/go.mod
@@ -3,4 +3,5 @@ module go-test
 go 1.14
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 )
---
+++ app/printer.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/davecgh/go-spew/spew"
+
+func Printer(s string) {
+	spew.Dump(s)
+}
